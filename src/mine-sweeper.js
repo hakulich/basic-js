cb00ca152bbddf471f6e33b5466898bf906a4a86
hakulich <71820310+hakulich@users.noreply.github.com>
2023-12-24 17:09:47 +0100

    feat: add mine sweerer

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,68 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let mines = [...matrix];
+
+  // Add mines
+  for (let i = 0; i < mines.length; i++) {
+    for (let j = 0; j < mines[i].length; j++) {
+      if (mines[i][j] === true) {
+        mines[i][j] = "X";
+      } else {
+        mines[i][j] = 0;
+      }
+    }
+  }
+
+  // calculate around mine
+
+  for (let i = 0; i < mines.length; i++) {
+    for (let j = 0; j < mines[i].length; j++) {
+      if (mines[i][j] === "X") {
+        if (j + 1 < mines[i].length && mines[i][j + 1] != "X") {
+          mines[i][j + 1] += 1;
+        }
+        if (i + 1 < mines.length && mines[i + 1][j] != "X") {
+          mines[i + 1][j] += 1;
+        }
+        if (i + 1 < mines.length && j + 1 < mines[i].length && mines[i + 1][j + 1] != "X") {
+          mines[i + 1][j + 1] += 1;
+        }
+
+        if (j - 1 >= 0 && mines[i][j - 1] != "X") {
+          mines[i][j - 1] += 1;
+        }
+        if (i - 1 >= 0 && mines[i - 1][j] != "X") {
+          mines[i - 1][j] += 1;
+        }
+        if (i - 1 >= 0 && j - 1 >= 0 && mines[i - 1][j - 1] != "X") {
+          mines[i - 1][j - 1] += 1;
+        }
+
+        if (i - 1 >= 0 && j + 1 < mines[i].length && mines[i - 1][j + 1] != "X") {
+          mines[i - 1][j + 1] += 1;
+        }
+
+        if (i + 1 < mines.length && j - 1 >= 0 && mines[i + 1][j - 1] != "X") {
+          mines[i + 1][j - 1] += 1;
+        }
+      }
+    }
+  }
+
+  // change mines
+  for (let i = 0; i < mines.length; i++) {
+    for (let j = 0; j < mines[i].length; j++) {
+      if (mines[i][j] === "X") {
+        mines[i][j] = 1;
+      }
+    }
+  }
+
+  return mines;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
