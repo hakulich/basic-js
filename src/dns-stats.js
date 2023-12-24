90b69de3dfb0d464f91a1f2be1c3493d94f7f523
hakulich <71820310+hakulich@users.noreply.github.com>
2023-12-24 14:13:20 +0100

    feat: add DNS stat

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let dns = {};
+
+  let arr = domains.map((elm) => elm.split(".").reverse()).map((elm) => elm.map((e) => `.${e}`));
+
+  arr.forEach((elm, index) => {
+    let domain = "";
+    for (let i = 0; i < elm.length; i++) {
+      domain += elm[i];
+      if (dns.hasOwnProperty(domain)) {
+        dns[domain] += 1;
+      } else {
+        dns[domain] = 1;
+      }
+    }
+  });
+
+  return dns;
 }
 
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
